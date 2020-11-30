6b9b6b32593958bfa29af4972c2e6d0c39400593
karastojko <contact@alepho.com>
2020-11-30 10:48:54 +0100

    Beautifying the JavaScript section.

--- emacs/init.el
+++ emacs/init.el
@@ -35,16 +35,18 @@
         (make-local-variable 'js-indent-level)
         (setq js-indent-level 4)))
 
-
 ; javascript
 (setq js2-basic-offset 4)
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(json-mode-indent-level 4)
- '(package-selected-packages (quote (json-mode markdown-mode magit js2-mode))))
+    ;; custom-set-variables was added by Custom.
+    ;; If you edit it by hand, you could mess it up, so be careful.
+    ;; Your init file should contain only one such instance.
+    ;; If there is more than one, they won't work right.
+    '(json-mode-indent-level 4)
+    '(js2-basic-offset 2)
+    '(js2-bounce-indent-p t)
+    '(package-selected-packages (quote (json-mode markdown-mode magit js2-mode))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
